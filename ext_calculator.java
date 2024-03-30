30e7f8fc3845f4978b10eb7ab3dcdde16816d417
varsha <01fe22bca019@kletech.ac.in>
2024-03-30 23:57:36 +0530

    add 2 files

---
+++ ext_calculator.java
@@ -0,0 +1,15 @@
+public class ext_calculator {
+
+    static float square(float x) {
+
+        return (x * x);
+
+    }
+
+    static float cube(float x) {
+
+        return (x * x * x);
+
+    }
+
+}
